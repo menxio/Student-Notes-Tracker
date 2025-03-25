b7e7721d0a3c33ebc24b74acffc9f42d805e86e3
menxio <jayceerubiato@gmail.com>
2025-03-25 18:44:41 +0800

    Successful rendering of sidebar

--- src/components/molecules/SidebarMenu.jsx
+++ src/components/molecules/SidebarMenu.jsx
@@ -1,36 +1,52 @@
-import React, { useState } from 'react';   
-import SchoolIcon from '@mui/icons-material/School';
+import React, { useState } from "react";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import SchoolIcon from "@mui/icons-material/School";
+import { Link } from "react-router-dom";
 
 function SidebarMenu() {
+  const links = [
+    {
+      label: "Freshman",
+      icon: <SchoolIcon />,
+      path: "/pages/Freshman",
+    },
+    {
+      label: "Sophomore",
+      icon: <SchoolIcon />,
+      path: "/pages/Sophomore",
+    },
+    {
+      label: "Junior",
+      icon: <SchoolIcon />,
+      path: "/pages/Junior",
+    },
+    {
+      label: "Senior",
+      icon: <SchoolIcon />,
+      path: "/pages/Senior",
+    },
+  ];
 
-    const links = [
-        {
-            label: 'Freshman',
-            icon: <SchoolIcon />,
-            path: '/pages/Freshman',
-        },
-        {
-            label: 'Sophomore',
-            icon: <SchoolIcon />,
-            path: '/pages/Sophomore',
-        },
-        {
-            label: 'Junior',
-            icon: <SchoolIcon />,
-            path: '/pages/Junior',
-        },
-        {
-            label: 'Senior',
-            icon: <SchoolIcon />,
-            path: '/pages/Senior',
-        },
-    ]
-    
-    return (
-        <>
-        
-        </>
-    )
+  return (
+    <List>
+      {links.map((link, index) => (
+        <ListItem key={index} disablePadding>
+          <ListItemButton component={Link} to={link.path}>
+            <ListItemIcon>
+              <SchoolIcon />
+            </ListItemIcon>
+            <ListItemText primary={link.label} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
 }
 
 export default SidebarMenu;
